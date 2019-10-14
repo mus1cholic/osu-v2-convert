2cd14c4aecc7b15aa27f46bd2538e987e5d2407d
Orion Hsu <orion.hsu@duke.edu>
2019-10-14 02:24:17 -0400

    Added sample .osu and replay files

--- parse.py
+++ parse.py
@@ -1,6 +1,6 @@
 from osrparse import parse_replay_file
 
-path = '/Users/Orion/AppData/Local/osu!/Replays/ - Mike Greene - Bill Nye the Science Guy Theme Song (Chinese Intro) [Easy] (2019-10-14) Osu.osr'
+path = '- Mike Greene - Bill Nye the Science Guy Theme Song (Chinese Intro) [Easy] (2019-10-14) Osu.osr'
 parse = parse_replay_file(path)
 play_data = parse.play_data
 
