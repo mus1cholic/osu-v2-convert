6d06abbadffb3333f857e07d884c7273e1a91bdc
Orion Hsu <orion.hsu@duke.edu>
2019-10-14 02:19:33 -0400

    Testing format of hit objects

---
+++ parse.py
@@ -0,0 +1,19 @@
+from osrparse import parse_replay_file
+
+path = '/Users/Orion/AppData/Local/osu!/Replays/ - Mike Greene - Bill Nye the Science Guy Theme Song (Chinese Intro) [Easy] (2019-10-14) Osu.osr'
+parse = parse_replay_file(path)
+play_data = parse.play_data
+
+time = 0
+
+for obj in play_data:
+    #print(str(obj.time_since_previous_action) + " " + str(obj.x)
+          #+ " " + str(obj.y) + " " + str(obj.keys_pressed))
+
+    if obj.keys_pressed == 5:
+        print(time)
+        break
+    
+    time += obj.time_since_previous_action
+
+#print(parse)
